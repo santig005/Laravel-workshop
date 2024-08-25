9d72b302d7b272aa13d52db08c67c1038f3b9ea8
santig005 <sajhdg30@gmail.com>
2024-08-24 19:54:52 -0500

    clean code corrections

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,29 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+
+    public function about(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'About us - Online Store';
+        $viewData['subtitle'] = 'About us';
+        $viewData['description'] = 'This is a description of the about page';
+        $viewData['author'] = 'Developed by: Santiago';
+
+        return view('home.about')->with('viewData', $viewData);
+    }
+
+    public function contact(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Contact us - Online Store';
+        $viewData['subtitle'] = 'Contact us';
+        $viewData['description'] = 'These are the means you can use to communicate with us ...';
+        $viewData['author'] = 'Developed by: Santiago';
+        $viewData['email'] = 'Email: saj@gmail.com';
+        $viewData['address'] = 'Address: Medellín - Co';
+        $viewData['phone_number'] = 'Phone number: +57 123 456 7890';
+
+        return view('home.contact')->with('viewData', $viewData);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,33 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
-Route::get('/about', function () {
-    $data1 = 'About us - Online Store';
-    $data2 = 'About us';
-    $description = 'This is an about page ...';
-    $author = 'Developed by: Santiago';
-
-    return view('home.about')->with('title', $data1)
-        ->with('subtitle', $data2)
-        ->with('description', $description)
-        ->with('author', $author);
-})->name('home.about');
-Route::get('/contact', function () {
-    $data1 = 'Contact us - Online Store';
-    $data2 = 'Contact us';
-    $description = 'These are the means you can use to communicate with us ...';
-    $author = 'Developed by: Santiago';
-    $email = 'Email: saj@gmail.com';
-    $address = 'Address: Medellín - Co';
-    $phone_number = 'Phone number: +57 123 456 7890';
-
-    return view('home.contact')->with('title', $data1)
-        ->with('subtitle', $data2)
-        ->with('description', $description)
-        ->with('email', $email)
-        ->with('address', $address)
-        ->with('phone_number', $phone_number);
-})->name('home.contact');
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
+Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
